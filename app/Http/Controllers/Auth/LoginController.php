4f8b353f8863cc3ba3c2793d4728addb6d163dd5
hnfarif <hnfarif18@gmail.com>
2023-12-17 10:08:28 +0700

    modify login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,17 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function username()
+    {
+        return 'id';
+    }
+
+
+    // public function login(Request $request)
+    // {
+    //     $this->validate($request, [
+    //         'id' => ''
+    //     parent::login($request);
+    // }
 }
