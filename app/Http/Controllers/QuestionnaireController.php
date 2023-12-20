304768864b17bb5a10df447e1bace319fcd3095b
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-12-20 07:24:23 +0700

    add new line 😂

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -30,46 +30,8 @@ class QuestionnaireController extends Controller
     {
         $questionnaire = Questionnaire::findOrFail($id);
         $categories = Category::all();
+
         return view('questionnaire.detail', compact('questionnaire', 'categories'));
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function question(string $id): View
-    {
-        $questionnaire = Questionnaire::findOrFail($id);
-        $startDate = (new DateTime($questionnaire->start_date))->getTimestamp();
-        $endDate = (new DateTime($questionnaire->end_date))->getTimestamp();
-        $isActive = time() >= $startDate && time() <= $endDate;
-
-        $questions = Question::where('questionnaire_id', '=', $questionnaire->id)->get();
-
-        if ($questionnaire->status != 'APPROVED' || !$isActive) {
-            abort(404);
-        }
-
-        return view('questionnaire.question', compact('questionnaire', 'questions'));
-    }
-
-    public function answer(Request $request): RedirectResponse
-    {
-        $student = Auth::user()->student;
-
-        foreach ($request->all() as $questionId => $scale) {
-            if (!str_starts_with($questionId, 'question-id-'))
-                continue;
-            $questionId = explode('question-id-', $questionId)[1];
-
-            // dd($scale);
-            Answer::create([
-                'question_id' => $questionId,
-                'student_id' => $student->nim,
-                'scale' => $scale
-            ]);
-        }
-
-        return back();
     }
 
     public function submit(string $id): RedirectResponse
